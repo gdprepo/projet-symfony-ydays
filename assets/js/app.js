5825ba744cca0433d5ac5e410296a36efb84e36c
dgabin <depairegabin@MacBook-Pro.local>
2020-10-14 16:45:06 +0200

    ajout bootstrap + login + logout + register

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+	
+import '../scss/app.scss';
+import 'bootstrap';
+// import 'bootstrap/dist/js/bootstrap';
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
+// import 'jquery'
+
+// const $ = require('jquery');
+// // this "modifies" the jquery module: adding behavior to it
+// // the bootstrap module doesn't export/return anything
+// require('bootstrap');
+// require('bootstrap/dist/js/bootstrap');
+
+
+// // or you can include specific pieces
+// // require('bootstrap/js/dist/tooltip');
+// // require('bootstrap/js/dist/popover');
+
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
